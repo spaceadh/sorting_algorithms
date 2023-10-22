2732a31a1215270bc8ff2e5218d4d1a575ed3cdb
spaceadh <spacesealadhesives13@gmail.com>
2023-10-22 01:52:46 -0700

    FileDeclarationsUptoMergeSort

---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @size: size of the array
+ * @array: list with numbers
+ */
+void selection_sort(int *array, size_t size)
+{
+
+}
