b0cc00eff9dd295798d9072d48e688ec20e35f02
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-25 03:31:22 +0000

    updated header file

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void q_s(int *array, ssize_t first, ssize_t last, int size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
