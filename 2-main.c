1ea8416f980d33d9edab12c346598e8ae0541c98
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-25 03:18:32 +0000

    2 main file

--- 2-main.c
+++ 2-main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
@@ -7,6 +5,7 @@
  *
  * Return: Always 0
  */
+ 
 int main(void)
 {
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
