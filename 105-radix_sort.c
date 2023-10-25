24969658123f1d5fd628ef78a02b6ac0fe5388cd
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-25 04:21:41 +0000

    radix sort file

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,11 +1,12 @@
 #include "sort.h"
-#include <stdio.h>
+
 /**
-* radix_sort - The main function to that sorts arr[]
-* of size n using Radix Sort
-* @array: array
-* @size: size of the array
+ * radix_sort - The main function to that sorts arr[]
+ * of size n using Radix Sort
+ * @array: array
+ * @size: size of the array
 */
+
 void radix_sort(int *array, size_t size)
 {
 
