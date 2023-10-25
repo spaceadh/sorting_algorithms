37ddc911e5e9bbaa1bc06a4df5640cc4ddf0a1b0
omotomiwa26 <omotomiwa26@gmail.com>
2023-10-25 02:49:29 +0000

    function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,33 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
+ * bubble_sort - This function sorts array elements
+ * from min to max value using burble sort
+ * @array: array to sort
  * @size: array size
  */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int tmp;
 
 	if (size < 2)
-    		return;
+		return;
 
-	for (i = 0; i < size - 1; i++) {
-    		for (j = 0; j < size - i - 1; j++) {
-        		if (array[j] > array[j + 1]) {
-            			tmp = array[j];
-            			array[j] = array[j + 1];
-            			array[j + 1] = tmp;
-            			print_array(array, size);
-        }
-    }
-}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 
-	
+
 }
